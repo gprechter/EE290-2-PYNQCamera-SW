6a51abc6009a2105a6d107194c0da0444f8df224
dylanbrater326 <43054934+dylanbrater326@users.noreply.github.com>
2021-05-03 11:36:16 -0700

    Create convolution.c

---
+++ convolution.c
@@ -0,0 +1,12 @@
+#define V_MATMULS 118
+#define H_MATMULS 158
+
+int main() {
+  int v;
+  int h;
+  for (v = 0; v < V_MATMULS; v++) {
+    for (h = 0; h < H_MATMMULS; h++) {
+        
+    }
+  }
+}
